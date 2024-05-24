66a269ac8e3bf64d33a7ac82f61f91c78fa9c86d
Nathanael Buendia <futiax@gmail.com>
2024-05-24 21:27:57 +0000

    0011

--- worker.ts
+++ worker.ts
@@ -1,18 +1,16 @@
-console.log("Loading worker")
+import { appendFile } from "node:fs/promises";
 self.addEventListener("message", event => {
     let [n, istart] = event.data.split(";");
-    console.log("Spawned worker with id " + istart)
     n = parseInt(n), istart = parseInt(istart);
     main(n, istart);
 });
 
 
 
-function main(n, istart) {
-    import { appendFile } from "node:fs/promises";
+async function main(n, istart) {
     const max = 100000;
     for (let i = istart; i < max; i+=n) {
-        let code = "0".repeat(5-String(i).length) + String(i)
+        let code = "0".repeat(5-String(i).length) + String(i);
         await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
     }
 }
