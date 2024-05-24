0f4e013c12588c83a703a9e5015bf3b1f1a89320
Nathanael Buendia <futiax@gmail.com>
2024-05-24 20:07:24 +0000

    Co-authored-by: kamil-leys <kamil-leys@users.noreply.github.com>

--- scan.ts
+++ scan.ts
@@ -1,6 +1,16 @@
 import { appendFile } from "node:fs/promises";
-for (let i = 19089; i < 100000; i++) {
+const istart = (await Bun.file('scan.txt').text()).split('\n').length;
+if (istart%2 == 1){
+    istart -=1
+}
+const workerURL = new URL("worker.ts", import.meta.url).href;
+const worker = new Worker(workerURL);
+let a = performance.now()
+const max = 100000;
+
+for (let i = istart; i < max; i++) {
     let code = "0".repeat(5-String(i).length) + String(i) 
-    console.log(code)
-    await appendFile("scan.txt", `${code}` + ": " + (await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName + "\n");
+    process.stdout.write('\x1Bc');
+    console.log(`vitesse :${(performance.now()-a)/(1000*(i-istart))}, progression : ${i+1}/${max}`);
+    await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
 }
---
+++ worker.ts
@@ -0,0 +1,13 @@
+import { appendFile } from "node:fs/promises";
+let istart = (await Bun.file('scan.txt').text()).split('\n').length;
+let a = performance.now()
+const max = 100000;
+
+if (istart % 2 == 0) istart++;
+
+for (let i = istart; i < max; i+=2) {
+    let code = "0".repeat(5-String(i).length) + String(i) 
+    process.stdout.write('\x1Bc');
+    console.log(`vitesse :${(performance.now()-a)/(1000*(i-istart))}, progression : ${i+1}/${max}`);
+    await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
+}
