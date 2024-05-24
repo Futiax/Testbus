8df0d864d36bbc94a5bea63f3bca42bb6743fe14
Nathanael Buendia <futiax@gmail.com>
2024-05-24 21:26:42 +0000

    Co-authored-by: kamil-leys <kamil-leys@users.noreply.github.com>

--- scan.ts
+++ scan.ts
@@ -1,16 +1,19 @@
 import { appendFile } from "node:fs/promises";
 let istart = (await Bun.file('scan.txt').text()).split('\n').length;
-if (istart%2 === 1){
-    istart -=1
-}
+let n = 4
 const workerURL = new URL("worker.ts", import.meta.url).href;
-const worker = new Worker(workerURL);
-let a = performance.now();
+for (let j = 1; j< n ; j++){
+    const worker = new Worker(workerURL);
+    worker.postMessage(`${n};${istart+j}`);
+    worker.onerror = (ev) => {
+        console.error(`Worker crashed with error: `)
+    }
+}
 const max = 100000;
-
-for (let i = istart; i < max; i+=2) {
+let a = performance.now();
+for (let i = istart; i < max; i+=n) {
     let code = "0".repeat(5-String(i).length) + String(i) 
-    process.stdout.write('\x1Bc');
-    console.log(`vitesse :${(performance.now()-a)/(1000*(i-istart))}, progression : ${((i+1)*100)/max}%(${i+1}/${max})`);
+    // process.stdout.write('\x1Bc');
+    console.log(`duré moyenne d'une itération :${(performance.now()-a)/(1000*(i-istart))}, progression : ${((i+1)*100)/max}%(${i+1}/${max})`);
     await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
 }
--- worker.ts
+++ worker.ts
@@ -1,11 +1,18 @@
-import { appendFile } from "node:fs/promises";
-let istart = (await Bun.file('scan.txt').text()).split('\n').length;
-let a = performance.now()
-const max = 100000;
+console.log("Loading worker")
+self.addEventListener("message", event => {
+    let [n, istart] = event.data.split(";");
+    console.log("Spawned worker with id " + istart)
+    n = parseInt(n), istart = parseInt(istart);
+    main(n, istart);
+});
 
-if (istart % 2 === 0) istart++;
 
-for (let i = istart; i < max; i+=2) {
-    let code = "0".repeat(5-String(i).length) + String(i)
-    await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
+
+function main(n, istart) {
+    import { appendFile } from "node:fs/promises";
+    const max = 100000;
+    for (let i = istart; i < max; i+=n) {
+        let code = "0".repeat(5-String(i).length) + String(i)
+        await appendFile("scan.txt", `${code}` + " : " + ((await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName||"Nothing") + "\n");
+    }
 }
