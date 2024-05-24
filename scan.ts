ec7d6ee7d617811353ce8103e0ab861110406e5e
Nathanael Buendia <futiax@gmail.com>
2024-05-24 19:41:32 +0000

    10101

--- scan.ts
+++ scan.ts
@@ -1,8 +1,6 @@
 import { appendFile } from "node:fs/promises";
 for (let i = 19089; i < 100000; i++) {
-    let a = performance.now()
     let code = "0".repeat(5-String(i).length) + String(i) 
     console.log(code)
-    await appendFile("scan.txt", `${code}` + ": " + (await (await fetch(`https://futiax.deno.dev/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName + "\n");
-    await Bun.sleep(550-(performance.now()-a))
+    await appendFile("scan.txt", `${code}` + ": " + (await (await fetch(`https://api11.stga.fr/saesi-ws/getHoursByStopCode.php?stopCode=${code}`)).json()).infos[0].stop.stopName + "\n");
 }
